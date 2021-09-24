c70654c953f6496310741d8b569f89e9d5e5f8f6
snowborodist <snowborodist@gmail.com>
2021-09-24 22:09:31 +0300

    added B-8-A, not confirmed

---
+++ Division_B/Homework_8/A.py
@@ -0,0 +1,66 @@
+def add(root, key):
+    if not root:
+        root.extend([key, None, None])
+        print("DONE")
+        return
+
+    root_key = root[0]
+    if key == root_key:
+        print("ALREADY")
+    elif key < root_key:
+        left_anc = root[1]
+        if not left_anc:
+            root[1] = [key, None, None]
+            print("DONE")
+        else:
+            add(root[1], key)
+    else:
+        right_anc = root[2]
+        if not right_anc:
+            root[2] = [key, None, None]
+            print("DONE")
+        else:
+            add(root[2], key)
+
+
+def search(root, key):
+    root_key = root[0]
+    if key == root_key:
+        print("YES")
+    elif key < root_key:
+        left_anc = root[1]
+        if not left_anc:
+            print("NO")
+        else:
+            search(left_anc, key)
+    else:
+        right_anc = root[2]
+        if not right_anc:
+            print("NO")
+        else:
+            add(root[2], key)
+
+
+def walk(root, depth=0):
+    if root[1]:
+        walk(root[1], depth + 1)
+    print(f"{''.join('.' * depth)}{root[0]}")
+    if root[2]:
+        walk(root[2], depth + 1)
+
+
+file = open('input.txt')
+commands = file.readlines()
+file.close()
+
+tree = []
+
+for command in commands:
+    command_parts = command.split()
+
+    if command_parts[0] == 'ADD':
+        add(tree, command_parts[1])
+    elif command_parts[0] == 'SEARCH':
+        search(tree, command_parts[1])
+    elif command_parts[0] == 'PRINTTREE':
+        walk(tree)
