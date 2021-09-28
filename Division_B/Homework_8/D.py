92ec93ed7b212d547699e87ebcf35ff51e592f87
snowborodist <snowborodist@gmail.com>
2021-09-28 19:22:51 +0300

    finished B-8-D

--- Division_B/Homework_8/D.py
+++ Division_B/Homework_8/D.py
@@ -1,50 +1,68 @@
-file = open('input_d_10.txt')
+# TODO: Не забудь поменять ввод на 'input.txt'
+file = open('input_d_12.txt')
 lines = file.readlines()
 file.close()
 
 n = int(lines[0])
 rel_map = {}
-nodes_set = set()
 
+# 1. Заполняем map со связями между узлами:
 for i in range(1, n):
     parent, child = map(int, lines[i].split())
-
-    nodes_set.add(parent)
-    nodes_set.add(child)
 
     if parent not in rel_map:
         rel_map[parent] = [child]
     else:
         rel_map[parent].append(child)
 
+    # Заполняем связи в обратную сторону,
+    # так как по условию у нас неориентированный граф:
     if child not in rel_map:
         rel_map[child] = [parent]
     else:
         rel_map[child].append(parent)
 
-# print(rel_map)
+
+# 2. Определяем функцию поиска листов дерева
+#    (или по-другому - конечных точек нецикличного неориентированного графа).
+#    Признак листа - всего одна связь с другим узлом.
+def get_leaves():
+    leaves_list = []
+    for node in rel_map:
+        if len(rel_map[node]) == 1:
+            leaves_list.append(node)
+    return leaves_list
 
 
-def find_max_route(start_node, relations):
+# 3. Определяем функцию поиска диаметра дерева (наибольшего пути графа).
+#    Идея заключается в том, чтобы каждый раз находить листы дерева,
+#    затем удалять их из дерева и добавлять к искомому значению 2,
+#    пока в дереве не останется меньше двух элементов.
+#    После этого прибавляем еще 1, если остался один (нечетный корневой) элемент, и выводим результат.
+#
+#    Тут стоит отдельно отметить, что наибольший путь в таком дереве (или ацикличном ненаправленном графе)
+#    это всегда путь от одного листа до другого через его корень.
+#    Прибавляем 2, а не 1, потому что удаляя листья, мы на каждом этапе
+#    сокращаем наш искомый наибольший путь сразу на 2 элемента.
 
-    depth = 0
-    v_nodes = set()
-    v_nodes.add(start_node)
-    nodes_to_process_next = relations[start_node]
+def find_diameter():
+    global rel_map
+    dist = 0
 
-    while nodes_to_process_next:
-        np_nodes = []
-        for p_node in nodes_to_process_next:
-            if p_node not in v_nodes:
-                v_nodes.add(p_node)
-                np_nodes.extend(relations[p_node])
-        nodes_to_process_next = np_nodes
-        depth += 1
+    while len(rel_map) >= 2:
+        leaves = get_leaves()
+        for leaf in leaves:
+            for leaf_child in rel_map[leaf]:
+                rel_map[leaf_child].remove(leaf)
+            rel_map.pop(leaf)
+        if len(leaves) < 2:
+            dist += 1
+        else:
+            dist += 2
 
-    return depth
+    dist += len(rel_map)
+
+    print(dist)
 
 
-distances = []
-for node in nodes_set:
-    distances.append(find_max_route(node, rel_map))
-print(max(distances))
+find_diameter()
